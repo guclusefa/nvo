bd34e049126ecaed3784c65b11a9986c6e63f75d
Sefa GUCLU <sefaaguclu@gmail.com>
2021-12-07 02:46:57 +0100

    dark theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
-// Add slideDown animation to Bootstrap dropdown when expanding.
+// animation navbar
 $('.dropdown').on('show.bs.dropdown', function () {
   $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
 });
-
-// Add slideUp animation to Bootstrap dropdown when collapsing.
 $('.dropdown').on('hide.bs.dropdown', function () {
   $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
 
+// dark mode
 var darkSwitch = document.getElementById("darkSwitch"); window.addEventListener("load", (function () { if (darkSwitch) { initTheme(); darkSwitch.addEventListener("change", (function () { resetTheme() })) } })); function initTheme() { var darkThemeSelected = localStorage.getItem("darkSwitch") !== null && localStorage.getItem("darkSwitch") === "dark"; darkSwitch.checked = darkThemeSelected; darkThemeSelected ? document.body.setAttribute("data-theme", "dark") : document.body.removeAttribute("data-theme") } function resetTheme() { if (darkSwitch.checked) { document.body.setAttribute("data-theme", "dark"); localStorage.setItem("darkSwitch", "dark") } else { document.body.removeAttribute("data-theme"); localStorage.removeItem("darkSwitch") } }
