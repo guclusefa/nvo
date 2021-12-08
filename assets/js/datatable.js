76c8f50b8f0413e0d4cc53fe604495e298be09c2
Sefa GUCLU <sefaaguclu@gmail.com>
2021-12-08 18:58:32 +0100

    datatable

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -1,22 +1,45 @@
 $(document).ready(function () {
-/*     $('#dataTable').DataTable({
-        dom: 'PQlfrtip',
-        responsive: true,
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json",
-            searchPlaceholder: "Rechercher...",
+    var fr = {
+        searchPanes: {
+            title: {
+                _: 'Filtres séléctionnés - %d',
+                0: 'Aucun filtre séléctionné',
+                1: 'Filtre séléctionné - %d'
+            }
         },
-        "bInfo": true,
-        "bLengthChange": false,
-        "pageLength": 25,
-    }); */
+        "sProcessing": "Traitement en cours...",
+        "sSearch": "Rechercher&nbsp;:",
+        "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
+        "sInfo": "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+        "sInfoEmpty": "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
+        "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+        "sInfoPostFix": "",
+        "sLoadingRecords": "Chargement en cours...",
+        "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+        "sEmptyTable": "Aucune donn&eacute;e disponible dans le tableau",
+        "oPaginate": {
+            "sFirst": "Premier",
+            "sPrevious": "Pr&eacute;c&eacute;dent",
+            "sNext": "Suivant",
+            "sLast": "Dernier"
+        },
+        "oAria": {
+            "sSortAscending": ": activer pour trier la colonne par ordre croissant",
+            "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
+        },
+        "select": {
+            "rows": {
+                "_": "%d lignes sélectionnées",
+                "0": "Aucune ligne sélectionnée",
+                "1": "1 ligne sélectionnée"
+            }
+        }
+    }
     var table = $('#dataTable').DataTable({
         searchPanes: true,
         responsive: true,
-        searchPanes: {
-/*             initCollapsed: true, */
-/*             clear: false */
-        }
+        pageLength: 25,
+        language: fr
     });
     table.searchPanes.container().prependTo(table.table().container());
     table.searchPanes.resizePanes();
