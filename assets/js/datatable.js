291f511c4da786c18aed0553d0cf91eee54c74a1
Sefa GUCLU <sefaaguclu@gmail.com>
2021-12-08 20:59:43 +0100

    modal

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -75,20 +75,20 @@ $(document).ready(function () {
 });
 
 
-function viewModal() {
+function viewModal(type, titre, desc, id) {
     modal = `
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel">${titre} ${id}</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+        ${desc}
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
           <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
